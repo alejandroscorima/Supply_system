eff050545dd39ca615d4fcc8a7e55e32e9a77fc3
Alejandro Oscorima <a20172445@pucp.edu.pe>
2023-01-02 15:37:37 -0500

    a

--- src/app/mobility/mobility.component.ts
+++ src/app/mobility/mobility.component.ts
@@ -789,7 +789,7 @@ export class DialogNewItemMobility implements OnInit {
   req: Requerimiento = new Requerimiento(null,null,null,null,null,null,null,[],null,'PENDIENTE',null);
 
   item: Item = new Item(null,null,null,'COMPRA','PENDIENTE','',null,'0','');
-  orden_item: OrdenItem = new OrdenItem(null,null,null,null,null,null);
+  orden_item: OrdenItem = new OrdenItem(null,null,null,null,null,null,null);
 
   listaReq: Item[]= [];
   categories:any= [];
@@ -921,7 +921,7 @@ export class DialogEditItemMobility implements OnInit {
   req: Requerimiento = new Requerimiento(null,null,null,null,null,null,null,[],null,'PENDIENTE',null);
 
   item: Item = new Item(null,null,null,'COMPRA','PENDIENTE','',null,'0','');
-  orden_item: OrdenItem = new OrdenItem(null,null,null,null,null,null);
+  orden_item: OrdenItem = new OrdenItem(null,null,null,null,null,null,null);
 
   listaReq: Item[]= [];
   categories:any= [];
