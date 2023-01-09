005e682b1a13184c577353961826b427e2710c5e
Alejandro Oscorima <a20172445@pucp.edu.pe>
2023-01-09 07:29:19 -0500

    release 9-1-22

--- server/postOrden.php
+++ server/postOrden.php
@@ -21,8 +21,8 @@ $jsonOrd = json_decode(file_get_contents("php://input"));
 try {
   $link = mysqli_connect('localhost', 'root', 'Oscorpsvr', 'oscorp_supply');
   $razSoc = mysqli_real_escape_string($link, $jsonOrd->razon_social);
-  mysqli_query($link, "INSERT INTO ordenes (req_id, numero, ruc, razon_social, direccion, subtotal, igv, total, rebajado, fecha, destino, tipo, estado, empresa, moneda, area, destino_dir, tipo_pago, num_cuenta) 
-  VALUES (".$jsonOrd->req_id.",'".$jsonOrd->numero."','".$jsonOrd->ruc."','".$razSoc."','".$jsonOrd->direccion."','".$jsonOrd->subtotal."','".$jsonOrd->igv."','".$jsonOrd->total."','".$jsonOrd->rebajado."','".$jsonOrd->fecha."','".$jsonOrd->destino."','".$jsonOrd->tipo."','".$jsonOrd->estado."','".$jsonOrd->empresa."','".$jsonOrd->moneda."','".$jsonOrd->area."','".$jsonOrd->destino_dir."','".$jsonOrd->tipo_pago."','".$jsonOrd->num_cuenta."')");
+  mysqli_query($link, "INSERT INTO ordenes (req_id, numero, ruc, razon_social, direccion, subtotal, igv, total, rebajado, fecha, destino, tipo, estado, empresa, moneda, area, destino_dir, tipo_pago, num_cuenta, retencion, retencion_percent, percepcion) 
+  VALUES (".$jsonOrd->req_id.",'".$jsonOrd->numero."','".$jsonOrd->ruc."','".$razSoc."','".$jsonOrd->direccion."','".$jsonOrd->subtotal."','".$jsonOrd->igv."','".$jsonOrd->total."','".$jsonOrd->rebajado."','".$jsonOrd->fecha."','".$jsonOrd->destino."','".$jsonOrd->tipo."','".$jsonOrd->estado."','".$jsonOrd->empresa."','".$jsonOrd->moneda."','".$jsonOrd->area."','".$jsonOrd->destino_dir."','".$jsonOrd->tipo_pago."','".$jsonOrd->num_cuenta."','".$jsonOrd->retencion."','".$jsonOrd->retencion_percent."','".$jsonOrd->percepcion."')");
   $id = mysqli_insert_id($link);
   echo json_encode([
       "session_id" => $id,
