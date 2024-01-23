c54f933acb2eee05aa59699ae4093ed14b06aa5e
Desarrollo01Q <development2.oscorp@gmail.com>
2024-01-23 16:03:29 -0500

    posEntrega.php

--- server/postEntregaItem.php
+++ server/postEntregaItem.php
@@ -22,7 +22,7 @@ if (!$jsonEntregaItem) {
 }
 $bd = include_once "bdLogistica.php";
 $sentencia = $bd->prepare("insert into entregaitems(campus,fecha,tipo_doc,serie,numero,ruc,raz_social,monto,categoria,estado,liquidacion_id, user_id, orden_id) values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
-$resultado = $sentencia->execute([$jsonEntregaItem->campus, $jsonEntregaItem->fecha, $jsonEntregaItem->tipo_doc, $jsonEntregaItem->serie, $jsonEntregaItem->numero, $jsojsonEntregaItemn->ruc, $jsonEntregaItem->raz_social, $jsonEntregaItem->monto, $jsonEntregaItem->categoria, $jsonEntregaItem->estado, $jsonEntregaItem->liquidacion_id, $jsonEntregaItem->user_id, $jsonEntregaItem->orden_id]);
+$resultado = $sentencia->execute([$jsonEntregaItem->campus, $jsonEntregaItem->fecha, $jsonEntregaItem->tipo_doc, $jsonEntregaItem->serie, $jsonEntregaItem->numero, $jsonEntregaItem->ruc, $jsonEntregaItem->raz_social, $jsonEntregaItem->monto, $jsonEntregaItem->categoria, $jsonEntregaItem->estado, $jsonEntregaItem->liquidacion_id, $jsonEntregaItem->user_id, $jsonEntregaItem->orden_id]);
 echo json_encode([
     "resultado" => $resultado,
 ]);
