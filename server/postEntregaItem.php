f8bb46a29d95cf45a8e59d758ab91d96a9acdbc9
Desarrollo01Q <development2.oscorp@gmail.com>
2024-01-23 15:54:12 -0500

    postEntrega.php

--- server/postEntregaItem.php
+++ server/postEntregaItem.php
@@ -16,13 +16,13 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 
 
 
-$jsonFondoItem = json_decode(file_get_contents("php://input"));
-if (!$jsonFondoItem) {
+$jsonEntregaItem = json_decode(file_get_contents("php://input"));
+if (!$jsonEntregaItem) {
     exit("No hay datos");
 }
 $bd = include_once "bdLogistica.php";
 $sentencia = $bd->prepare("insert into entregaitems(campus,fecha,tipo_doc,serie,numero,ruc,raz_social,monto,categoria,estado,liquidacion_id, user_id, orden_id) values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
-$resultado = $sentencia->execute([$jsonEntregaItem->campus, $jsonEntregaItem->fecha, $jsonEntregaItem->tipo_doc, $jsonEntregaItem->serie, $jsonEntregaItem->numero, $jsojsonEntregaItemnFondoItem->ruc, $jsonEntregaItem->raz_social, $jsonEntregaItem->monto, $jsonEntregaItem->categoria, $jsonEntregaItem->estado, $jsonEntregaItem->liquidacion_id, $jsonEntregaItem->user_id, $jsonEntregaItem->orden_id]);
+$resultado = $sentencia->execute([$jsonEntregaItem->campus, $jsonEntregaItem->fecha, $jsonEntregaItem->tipo_doc, $jsonEntregaItem->serie, $jsonEntregaItem->numero, $jsojsonEntregaItemn->ruc, $jsonEntregaItem->raz_social, $jsonEntregaItem->monto, $jsonEntregaItem->categoria, $jsonEntregaItem->estado, $jsonEntregaItem->liquidacion_id, $jsonEntregaItem->user_id, $jsonEntregaItem->orden_id]);
 echo json_encode([
     "resultado" => $resultado,
 ]);
