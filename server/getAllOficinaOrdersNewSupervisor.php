37a832794567e39807380414318daad905d33294
Mufment <jorleotuppiac@gmail.com>
2024-04-08 15:22:53 -0500

    miaurgf

--- server/getAllOficinaOrdersNewSupervisor.php
+++ server/getAllOficinaOrdersNewSupervisor.php
@@ -9,136 +9,95 @@ $user_id = $_GET['user_id'];
 $user_role = $_GET['user_role'];
 $destino = $_GET['destino'];
 
-// Definir la parte adicional para el LEFT JOIN y WHERE
-$left_join_clause = "LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
-                    WHERE c.user_id = '".$user_id."' OR c.user_id IS NULL";
-
 if ($user_role == 'SUPERVISOR' || $user_role == 'ADMINISTRADOR') {
     if ($destino == 'NINGUNO') {
-        $sentencia = $bd->prepare("SELECT a.*, 
+        $sentencia = $bd->prepare("SELECT a.*, COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante,
             c.id AS val_id,
             c.user_id AS val_user_id,
             c.order_id AS val_order_id,
             c.date AS val_date,
             c.hour AS val_hour,
-            c.state AS val_state,
-            COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante 
+            c.state AS val_state
             FROM (
                 SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
                 ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
                 ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
                 ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
-                FROM oscorp_supply.ordenes WHERE ordenes.section='OFICINA' AND ordenes.user_id=".$user_id." ) a 
+                FROM oscorp_supply.ordenes 
+                WHERE ordenes.section = 'OFICINA' AND ordenes.user_id = ".$user_id."
+            ) a 
             LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
-            ".$left_join_clause."
+            LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
+            WHERE c.user_id = '".$user_id."' OR c.user_id IS NULL
             ORDER BY a.id DESC;");
     } else {
-        if ($destino == 'TODOS') {
-            $sentencia = $bd->prepare("SELECT a.*, 
-                c.id AS val_id,
-                c.user_id AS val_user_id,
-                c.order_id AS val_order_id,
-                c.date AS val_date,
-                c.hour AS val_hour,
-                c.state AS val_state,
-                COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante 
-                FROM (
-                    SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion,
-                    ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
-                    ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
-                    ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
-                    FROM oscorp_supply.ordenes WHERE ordenes.section='OFICINA' ) a 
-                LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
-                ".$left_join_clause."
-                ORDER BY a.id DESC;");
-        } else {
-            $sentencia = $bd->prepare("SELECT a.*, 
-                c.id AS val_id,
-                c.user_id AS val_user_id,
-                c.order_id AS val_order_id,
-                c.date AS val_date,
-                c.hour AS val_hour,
-                c.state AS val_state,
-                COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante 
-                FROM (
-                    SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
-                    ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
-                    ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
-                    ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
-                    FROM oscorp_supply.ordenes WHERE ordenes.section='OFICINA' AND ordenes.destino='".$destino."' ) a 
-                LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
-                ".$left_join_clause."
-                ORDER BY a.id DESC;");
-        }
+        $sentencia = $bd->prepare("SELECT a.*, COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante,
+            c.id AS val_id,
+            c.user_id AS val_user_id,
+            c.order_id AS val_order_id,
+            c.date AS val_date,
+            c.hour AS val_hour,
+            c.state AS val_state
+            FROM (
+                SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion,
+                ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
+                ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
+                ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
+                FROM oscorp_supply.ordenes 
+                WHERE ordenes.section = 'OFICINA' AND ordenes.destino = '".$destino."'
+            ) a 
+            LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
+            LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
+            WHERE c.user_id = '".$user_id."' OR c.user_id IS NULL
+            ORDER BY a.id DESC;");
+    }
+} elseif ($user_role == 'SUPER ADMINISTRADOR') {
+    if ($destino == 'NINGUNO') {
+        $sentencia = $bd->prepare("SELECT * FROM oscorp_supply.ordenes WHERE ordenes.section='CASA';");
+    } else {
+        $sentencia = $bd->prepare("SELECT a.*, COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante,
+            c.id AS val_id,
+            c.user_id AS val_user_id,
+            c.order_id AS val_order_id,
+            c.date AS val_date,
+            c.hour AS val_hour,
+            c.state AS val_state
+            FROM (
+                SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
+                ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
+                ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
+                ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
+                FROM oscorp_supply.ordenes 
+                WHERE ordenes.section = 'OFICINA'
+            ) a 
+            LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
+            LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
+            WHERE c.user_id = '".$user_id."' OR c.user_id IS NULL
+            ORDER BY a.id DESC;");
     }
 } else {
-    if ($user_role == 'SUPER ADMINISTRADOR') {
-        if ($destino == 'NINGUNO') {
-            $sentencia = $bd->prepare("SELECT * FROM oscorp_supply.ordenes WHERE ordenes.section='CASA';");
-        } else {
-            if ($destino == 'TODOS') {
-                $sentencia = $bd->prepare("SELECT a.*, 
-                    c.id AS val_id,
-                    c.user_id AS val_user_id,
-                    c.order_id AS val_order_id,
-                    c.date AS val_date,
-                    c.hour AS val_hour,
-                    c.state AS val_state,
-                    COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante 
-                    FROM (
-                        SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
-                        ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.total_inicial, ordenes.fecha, 
-                        ordenes.destino, ordenes.tipo, ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, 
-                        ordenes.tipo_pago, ordenes.num_cuenta, ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, 
-                        ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
-                        FROM oscorp_supply.ordenes WHERE ordenes.section='OFICINA' ) a 
-                    LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
-                    ".$left_join_clause."
-                    ORDER BY a.id DESC;");
-            } else {
-                $sentencia = $bd->prepare("SELECT a.*, 
-                    c.id AS val_id,
-                    c.user_id AS val_user_id,
-                    c.order_id AS val_order_id,
-                    c.date AS val_date,
-                    c.hour AS val_hour,
-                    c.state AS val_state,
-                    COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante 
-                    FROM (
-                        SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
-                        ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.total_inicial, ordenes.fecha, 
-                        ordenes.destino, ordenes.tipo, ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, 
-                        ordenes.tipo_pago, ordenes.num_cuenta, ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, 
-                        ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
-                        FROM oscorp_supply.ordenes WHERE ordenes.section='OFICINA' AND ordenes.destino='".$destino."' ) a 
-                    LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
-                    ".$left_join_clause."
-                    ORDER BY a.id DESC;");
-            }
-        }
-    } else {
-        $sentencia = $bd->prepare("SELECT a.*, 
-            c.id AS val_id,
-            c.user_id AS val_user_id,
-            c.order_id AS val_order_id,
-            c.date AS val_date,
-            c.hour AS val_hour,
-            c.state AS val_state,
-            COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante 
-            FROM (
-                SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
-                ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
-                ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
-                ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
-                FROM oscorp_supply.ordenes WHERE ordenes.section='OFICINA' AND ordenes.user_id=".$user_id." ) a 
-            LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
-            ".$left_join_clause."
-            ORDER BY a.id DESC;");
-    }
+    $sentencia = $bd->prepare("SELECT a.*, COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante,
+        c.id AS val_id,
+        c.user_id AS val_user_id,
+        c.order_id AS val_order_id,
+        c.date AS val_date,
+        c.hour AS val_hour,
+        c.state AS val_state
+        FROM (
+            SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
+            ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
+            ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
+            ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
+            FROM oscorp_supply.ordenes 
+            WHERE ordenes.section = 'OFICINA' AND ordenes.user_id = ".$user_id."
+        ) a 
+        LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
+        LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
+        WHERE a.user_id = ".$user_id."
+        ORDER BY a.id DESC;");
 }
 
 $sentencia->execute();
 $orders = $sentencia->fetchAll(PDO::FETCH_OBJ);
 echo json_encode($orders);
-
 ?>
