553cbb2be459a20af4da2d1e7273925769b70e63
Mufment <jorleotuppiac@gmail.com>
2024-04-19 17:08:32 -0500

    fdfd

--- server/getOrdersbyStepStatus.php
+++ server/getOrdersbyStepStatus.php
@@ -92,35 +92,37 @@ if ($user_role == 'SUPERVISOR' || $user_role == 'ADMINISTRADOR') {
                     SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
                     ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
                     ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
-                    ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
+                    ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section,
+                    ordenes.status, ordenes.observacion,ordenes.step,ordenes.step_status 
                     FROM oscorp_supply.ordenes 
                     WHERE ordenes.section = 'OFICINA'
                 ) a 
                 LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
                 LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
-                AND c.user_id = '".$user_id."' 
+				WHERE a.step = 'STEP_1' AND a.step_status='PENDIENTE'
                 ORDER BY a.id DESC;");
             }
             else{
             $sentencia = $bd->prepare("SELECT a.*, COALESCE(CONCAT(b.serie, '-', b.numero), 'SN') AS comprobante,
-                c.id AS val_id,
-                c.user_id AS val_user_id,
-                c.order_id AS val_order_id,
-                c.date AS val_date,
-                c.hour AS val_hour,
-                c.state AS val_state
-                FROM (
-                    SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
-                    ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
-                    ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
-                    ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section, ordenes.status, ordenes.observacion 
-                    FROM oscorp_supply.ordenes 
-                    WHERE ordenes.section = 'OFICINA' AND ordenes.destino='".$destino."'
-                ) a 
-                LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
-                LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
-                AND c.user_id = '".$user_id."'
-                ORDER BY a.id DESC;");
+            c.id AS val_id,
+            c.user_id AS val_user_id,
+            c.order_id AS val_order_id,
+            c.date AS val_date,
+            c.hour AS val_hour,
+            c.state AS val_state
+            FROM (
+                SELECT ordenes.id, ordenes.req_id, ordenes.numero, ordenes.ruc, ordenes.razon_social, ordenes.direccion, 
+                ordenes.subtotal, ordenes.igv, ordenes.total, ordenes.rebajado, ordenes.fecha, ordenes.destino, ordenes.tipo,
+                ordenes.estado, ordenes.empresa, ordenes.moneda, ordenes.area, ordenes.destino_dir, ordenes.tipo_pago, ordenes.num_cuenta,
+                ordenes.retencion, ordenes.retencion_percent, ordenes.percepcion, ordenes.receipt, ordenes.txt, ordenes.section,
+                ordenes.status, ordenes.observacion,ordenes.step,ordenes.step_status 
+                FROM oscorp_supply.ordenes 
+                WHERE ordenes.section = 'OFICINA' AND ordenes.destino='".$destino."'
+            ) a 
+            LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
+            LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
+            WHERE a.step = 'STEP_1' AND a.step_status='PENDIENTE'
+            ORDER BY a.id DESC;");
             }
         }
     } else{
@@ -133,7 +135,7 @@ if ($user_role == 'SUPERVISOR' || $user_role == 'ADMINISTRADOR') {
         FROM oscorp_supply.ordenes WHERE  ordenes.section='OFICINA' AND ordenes.user_id=".$user_id." ) a 
         LEFT JOIN oscorp_supply.fondoitems b ON a.id = b.orden_id 
         LEFT JOIN oscorp_supply.orders_validations c ON a.id = c.order_id 
-        AND a.user_id = ".$user_id."
+
         ORDER BY a.id DESC;");
     }
 }
