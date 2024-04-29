42d614577c394d4eb4bee74e30270fd9cc1936d2
Mufment <jorleotuppiac@gmail.com>
2024-04-29 10:52:43 -0500

    "Removed unused bindParam statement in getFilesByOrdenId.php"
    Explanation:
    The code diff shows that a line of code binding a parameter to the SQL query has been removed. This suggests that the parameter binding

--- server/getFilesByOrdenId.php
+++ server/getFilesByOrdenId.php
@@ -7,7 +7,7 @@ $bd = include_once "bdLogistica.php";
 
 $sentencia = $bd->prepare("SELECT id, name, url, description, extension, date, hour, order_id, folder_id FROM oscorp_paperless.file WHERE order_id =".$orden_id." ");
 
-$sentencia->bindParam(':orden_id', $orden_id, PDO::PARAM_INT);
+
 
 $sentencia->execute();
 
