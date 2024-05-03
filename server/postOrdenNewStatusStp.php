0778b18f3e32ae5544a952aaf6f7769ee9b69da2
Mufment <jorleotuppiac@gmail.com>
2024-05-03 15:35:07 -0500

    fix 3

--- server/postOrdenNewStatusStp.php
+++ server/postOrdenNewStatusStp.php
@@ -12,6 +12,10 @@ $jsonOrd = json_decode(file_get_contents("php://input"));
 try {
     $link = mysqli_connect('localhost', 'root', 'Oscorpsvr', 'oscorp_supply');
     $razSoc = mysqli_real_escape_string($link, $jsonOrd->razon_social);
+    
+    // Verificar si se proporciona un valor para "status"
+    $status = isset($jsonOrd->status) ? $jsonOrd->status : 'pendiente';
+    
     mysqli_query($link, "INSERT INTO ordenes (req_id, numero, ruc, razon_social, direccion,
     subtotal, igv, total, rebajado, fecha, destino, tipo, estado, empresa, moneda, area,
     destino_dir, tipo_pago, num_cuenta, retencion, retencion_percent, percepcion, receipt, txt, section,
@@ -21,7 +25,7 @@ try {
     .$jsonOrd->destino."','".$jsonOrd->tipo."','".$jsonOrd->estado."','".$jsonOrd->empresa."','".$jsonOrd->moneda."','"
     .$jsonOrd->area."','".$jsonOrd->destino_dir."','".$jsonOrd->tipo_pago."','".$jsonOrd->num_cuenta."','".$jsonOrd->retencion."','"
     .$jsonOrd->retencion_percent."','".$jsonOrd->percepcion."','".$jsonOrd->receipt."','".$jsonOrd->txt."','".$jsonOrd->section."','"
-    .$jsonOrd->user_id."','".$jsonOrd->observacion."', '".$jsonOrd->status."')");
+    .$jsonOrd->user_id."','".$jsonOrd->observacion."', '".$status."')");
 
     $id = mysqli_insert_id($link);
     echo json_encode([
