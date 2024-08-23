1c5661747e9eedccc4a874782ba0a88b7917eb45
Mufment <jorleotuppiac@gmail.com>
2024-08-23 14:26:34 -0500

    getReqsByDate.php upload php

---
+++ server/getReqsByDate.php
@@ -0,0 +1,109 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$bd = include_once "bdLogistica.php";
+
+$user_role = $_GET['user_role'];
+$user_id = $_GET['user_id'];
+$status = $_GET['status'];
+$sede = $_GET['sede'];
+
+$start_date = $_GET['start_date']; // Fecha de inicio
+$end_date = $_GET['end_date']; // Fecha de fin
+$area = $_GET['area']; // Área
+
+$extraString = "";
+$validationExtraString = "";
+
+$dateFilter = " a.fecha BETWEEN '" . $start_date . "' AND '" . $end_date . "'";
+$areaFilter ="";
+if($area=="TODOS"){
+    $areaFilter="AND a.area ='".$area."'";
+}
+
+$sedeFilter ="";
+if($sede=="TODOS"){
+    $sedeFilter="AND a.sede ='".$sede."'";
+}
+
+if ($user_role == 'SUPER USUARIO') {
+    // Lógica específica para SUPER USUARIO
+}
+
+if ($user_role == 'SUPER ADMINISTRADOR') {
+    // Lógica específica para SUPER ADMINISTRADOR
+}
+
+if ($user_role == 'ADMINISTRADOR') {
+    // Lógica específica para ADMINISTRADOR
+    $extraString .= "AND rd.id_asignado = " . $user_id;
+}
+
+if ($user_role == 'ASISTENTE') {
+    // Lógica específica para ASISTENTE
+    $extraString .= "AND rd.id_asignado = " . $user_id;
+}
+
+if ($user_role == 'USUARIO AVANZADO') {
+    // Lógica específica para USUARIO AVANZADO
+    $extraString .= "AND a.user_id = " . $user_id;
+}
+
+if ($user_role == 'USUARIO') {
+    $extraString .= "AND a.user_id = " . $user_id;
+}
+
+if ($user_role == 'SUPERVISOR') {
+
+}
+
+$sentencia = $bd->prepare("
+SELECT * FROM
+(SELECT 
+    a.id, 
+    a.codigo, 
+    a.fecha, 
+    a.area, 
+    a.encargado, 
+    a.sala, 
+    a.prioridad, 
+    a.motivo, 
+    a.id_asignado, 
+    a.total_budget,
+    rd.id as rd_id,
+    COALESCE(b.id,0) AS validation_id,
+    CASE
+        WHEN COUNT(b.id) = 0 THEN 'NO CORRESPONDE'
+        WHEN COUNT(b.id) = 1 THEN MAX(b.state)
+        WHEN COUNT(b.id) > 1 AND SUM(b.state = 'PENDIENTE') > 0 THEN 'PENDIENTE'
+        WHEN COUNT(b.id) > 1 AND SUM(b.state = 'PENDIENTE') = 0 AND COUNT(DISTINCT b.state) = 1 THEN MAX(b.state)
+        ELSE 'CONFLICTO'
+    END AS validation,
+    CASE
+        WHEN SUM(CASE WHEN rd.estado = 'ENTREGADO' THEN 1 ELSE 0 END) > 0 THEN 'FINALIZADO'
+        WHEN SUM(CASE WHEN rd.estado IN ('ASIGNADO', 'COMPRADO') THEN 1 ELSE 0 END) > 0 THEN 'PROCESO'
+        WHEN SUM(CASE WHEN rd.estado = 'PENDIENTE' THEN 1 ELSE 0 END) > 0 THEN 'PENDIENTE'
+        ELSE 'NO DEFINIDO'
+    END AS estado
+FROM 
+    requerimientos a
+LEFT JOIN 
+    req_validations b ON a.id = b.req_id".$validationExtraString."
+LEFT JOIN 
+    req_detalles rd ON a.codigo = rd.req_codigo
+WHERE 
+    TRUE
+    
+    ".$extraString.$areaFilter. $sedeFilter.$dateFilter."
+
+
+GROUP BY rd.id
+ORDER BY 
+    a.id DESC) t
+GROUP BY t.id, t.estado
+");
+
+$sentencia->execute();
+$reqs = $sentencia->fetchAll(PDO::FETCH_OBJ);
+echo json_encode($reqs);
+?>
